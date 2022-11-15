e85d08e5179f026f3f22344a5beef4a353a0f56e
abrinkmann <alex.brinkmann@informatik.uni-mannheim.de>
2022-11-15 13:12:03 +0100

    Update augmentation

--- src/contrastive/data/augmentation.py
+++ src/contrastive/data/augmentation.py
@@ -1,10 +1,12 @@
 import random
 
+
 def delete_random_tokens(string_value):
-    """Deletes 10% of the tokens"""
+    """Deletes a single token"""
     tokens = string_value.split()
     # Calculate number of to be removed tokens
-    num_remove = int(len(tokens)/10) + 1
+    #num_remove = int(len(tokens)/10) + 1
+    num_remove = 1
     for _ in range(num_remove):
         tokens.pop(random.randint(0, len(tokens)-1))
 
